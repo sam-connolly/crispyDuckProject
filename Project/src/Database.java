1167daf45f89999a456d6dcfaf80a3c8476d898f
Petersweb <32275665+Petersweb@users.noreply.github.com>
2018-02-16 21:07:34 +0000

    Connected to db

--- Project/src/Database.java
+++ Project/src/Database.java
@@ -1,4 +1,28 @@
+import java.sql.*;
 
 public class Database {
-
+	private String driver = "jdbc:ucanaccess://";
+	private String Db = "database//crispyDuckDatabase.accdb";
+	private Connection conn = null;
+	private String url = driver + Db;
+	
+	public Database() {
+		connect();
+	}
+	public boolean connect() {
+		try {
+			conn = DriverManager.getConnection(url);
+			System.err.println("yay");
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+	
+	public static void main(String [] args) {
+		Database testDb = new Database();
+		} 
 }
