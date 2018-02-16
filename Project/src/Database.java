20eb6a50b5495136633bfed015820dd7a43c2a54
Petersweb <32275665+Petersweb@users.noreply.github.com>
2018-02-16 22:15:26 +0000

    Basic class that gets userID and name

--- Project/src/Database.java
+++ Project/src/Database.java
@@ -8,6 +8,7 @@ public class Database {
 	
 	public Database() {
 		connect();
+		getusers();
 	}
 	public boolean connect() {
 		try {
@@ -20,6 +21,51 @@ public class Database {
 			return false;
 		}
 		
+	}
+	
+	public void getusers() {
+		try {
+			Statement stmt = conn.createStatement();
+			String query = "SELECT UserID, EmployeeName FROM User";
+		    ResultSet rs = stmt.executeQuery (query);
+		    
+		    // position to first record
+		    boolean moreRecords = rs.next ();
+		    // If there are no records, display a message
+		    if (!moreRecords) {
+		      System.out.println ("ResultSet contained no records");
+		      return;
+		    }
+		    // display column headings
+		    ResultSetMetaData rsmd = rs.getMetaData ();
+		    for (int i = 1;  i <= rsmd.getColumnCount ();  i ++)
+		      System.out.print (rsmd.getColumnName (i) + "\t");
+		    System.out.println ();
+		    // display rows of data ....
+		    do {
+		      displayRow (rs, rsmd); //helper method below
+		    }while (rs.next ());
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		    finally {
+				System.out.println("done");
+			}
+		  }
+		private void displayRow (ResultSet rs, ResultSetMetaData rsmd) 	throws SQLException {
+		    for (int i = 1;  i <= rsmd.getColumnCount ();  i ++)
+		      switch (rsmd.getColumnType (i)) {
+		      case Types.VARCHAR:
+		      case Types.LONGVARCHAR:
+		        System.out.print (rs.getString (i)+"\t");  break;
+		      case Types.INTEGER:
+		      case Types.NUMERIC:
+		        System.out.print (""+ rs.getInt (i) + "\t");  break;
+		      default:	// do nothing – print nothing
+		      }
+		    System.out.println ();
+		  		
 	}
 	
 	public static void main(String [] args) {
