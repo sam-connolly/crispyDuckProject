31684fb0b8651c9b9adafd43a882203766388e50
Sam Connolly <sam.connolly@northumbria.ac.uk>
2018-04-13 22:51:48 +0100

    Added date input via comboBoxes. No longer creates tasks to database FIX

--- Project/src/TaskCreationUI.java
+++ Project/src/TaskCreationUI.java
@@ -5,6 +5,8 @@ import javax.swing.plaf.basic.BasicComboBoxRenderer;
 import java.awt.*;
 import java.util.ArrayList;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
 
@@ -109,6 +111,102 @@ public class TaskCreationUI extends JFrame
 		pnlDataEntry.add(txtLocation);
 		txtLocation.setColumns(10);
 		
+		JLabel lblDateDue = new JLabel("DateDue");
+		lblDateDue.setHorizontalAlignment(SwingConstants.CENTER);
+		pnlDataEntry.add(lblDateDue);
+		
+		JPanel pnlDateInput = new JPanel();
+		pnlDataEntry.add(pnlDateInput);
+		
+		JComboBox<Integer> cmbDateDay = new JComboBox<Integer>();
+		cmbDateDay.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
+		pnlDateInput.add(cmbDateDay);
+		
+		JLabel lblDateSeperator1 = new JLabel("/");
+		pnlDateInput.add(lblDateSeperator1);
+		
+		JComboBox<String> cmbDateMonth = new JComboBox<String>();
+		cmbDateMonth.setModel(new DefaultComboBoxModel<String>(new String[] {"January", "February", "March", "April", "May",
+				"June", "July", "August", "September", "October", "November", "December"}));
+		pnlDateInput.add(cmbDateMonth);
+				
+		JLabel lblDateSeperator2 = new JLabel("/");
+		pnlDateInput.add(lblDateSeperator2);
+		
+		JComboBox<Integer> cmbDateYear = new JComboBox<Integer>();
+		for (int i = 2018; i <= 3000; i++)
+		{
+			cmbDateYear.addItem(i);
+		}
+		pnlDateInput.add(cmbDateYear);
+		
+		cmbDateMonth.addItemListener(new ItemListener()
+		{
+			public void itemStateChanged(ItemEvent evt)
+			{
+				String month = (String) cmbDateMonth.getSelectedItem();
+				int year = (Integer) cmbDateYear.getSelectedItem();
+				
+				if (evt.getStateChange() == ItemEvent.SELECTED)
+				{
+					cmbDateDay.removeAllItems();
+					
+					if (month == "September" || month == "April" || month =="June" || month =="November")
+					{
+						for (int i = 1; i <= 30; i++)
+						{
+							cmbDateDay.addItem(i);
+						}
+					}
+					else if (cmbDateMonth.getSelectedItem() == "February")
+					{
+						if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+						{
+							for (int i = 1; i <= 29; i++)
+							{
+								cmbDateDay.addItem(i);
+							}
+						}
+						else
+						{
+							for (int i = 1; i <= 28; i++)
+							{
+								cmbDateDay.addItem(i);
+							}
+						}
+					}
+					else
+					{
+						for (int i = 0; i <=31; i++)
+						{
+							cmbDateDay.addItem(i);
+						}
+					}
+				}
+			}
+		});
+		
+		cmbDateYear.addItemListener(new ItemListener()
+		{
+			public void itemStateChanged(ItemEvent evt)
+			{
+				String month = (String) cmbDateMonth.getSelectedItem();
+				int year = (Integer) cmbDateYear.getSelectedItem();
+				
+				if (evt.getStateChange() == ItemEvent.SELECTED)
+				{				
+					if (month == "February" && ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))))
+					{
+						cmbDateDay.removeAllItems();
+						for (int i = 1; i <= 29; i++)
+						{
+							cmbDateDay.addItem(i);
+						}
+					}
+				}
+			}
+		});
+		
 		JLabel lblCategory = new JLabel("Category *");
 		lblCategory.setHorizontalAlignment(SwingConstants.CENTER);
 		pnlDataEntry.add(lblCategory);
@@ -185,7 +283,6 @@ public class TaskCreationUI extends JFrame
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
-			
 				int hours = (Integer) cmbHours.getSelectedItem();
 				int minutes = (Integer) cmbMinutes.getSelectedItem();
 				
@@ -199,16 +296,36 @@ public class TaskCreationUI extends JFrame
 				}
 				else
 				{
+					String month = (String) cmbDateMonth.getSelectedItem();
+					String monthN;
+					if (month == "October")
+					{
+						monthN = "10";
+					}
+					else if (month == "November")
+					{
+						monthN = "11";
+					}
+					else if (month == "December")
+					{
+						monthN = "12";
+					}
+					else
+					{
+						monthN = "0" + (cmbDateMonth.getSelectedIndex() + 1);
+					}
+					String dateDue = cmbDateDay.getSelectedItem() + "/" + monthN + "/" + cmbDateYear.getSelectedItem();
 					int timeEstimateInMinutes = (hours * 60) + minutes;
 					String insertSQL = "INSERT INTO Task (TaskName, TaskDesc,"
-							+ " TaskCat, Priority, Repeating, TimeEstimate, Location)"
+							+ " TaskCat, Priority, Repeating, TimeEstimate, Location, DateDue)"
 							+ "VALUES ('" + txtTaskName.getText() + "', '" 
 							+ txtaDescription.getText() + "', '"
 							+ cmbCategory.getSelectedItem() + "', '"
 							+ cmbPriority.getSelectedItem() + "', '"
 							+ cmbRepeatingDays.getSelectedItem() + "', '"
 							+ timeEstimateInMinutes + "', '"
-							+ txtLocation.getText() + "');";
+							+ txtLocation.getText() + "', '"
+							+ dateDue + "');";
 					
 					Boolean created = database.createTask(insertSQL);
 					
